05c5c23c3ce23b40a042ba87dcfc36c275298c90
mewmew <rnd0x00@gmail.com>
2015-01-26 15:41:18 +0100

    Watch "Hans Rosling: The best stats you've ever seen".

--- stash/2006-06.sh
+++ stash/2006-06.sh
@@ -1,3 +1,2 @@
 mkdir -p "2006-06"
-wget -O "2006-06/Hans Rosling: The best stats you've ever seen (TED2006) [19m50s].mp4" http://download.ted.com/talks/HansRosling_2006-480p.mp4?apikey=TEDDOWNLOAD
 wget -O "2006-06/Ken Robinson: How schools kill creativity (TED2006) [19m24s].mp4" http://download.ted.com/talks/SirKenRobinson_2006-480p.mp4?apikey=TEDDOWNLOAD
