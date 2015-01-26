8a06ce90cfe1b4da7600e87e1a7fe02633e26d62
mewmew <rnd0x00@gmail.com>
2015-01-26 15:13:40 +0100

    Watch "Tony Robbins: Why we do what we do" and "David Pogue: Simplicity sells".

--- stash/2006-06.sh
+++ stash/2006-06.sh
@@ -1,7 +1,5 @@
 mkdir -p "2006-06"
 wget -O "2006-06/Hans Rosling: The best stats you've ever seen (TED2006) [19m50s].mp4" http://download.ted.com/talks/HansRosling_2006-480p.mp4?apikey=TEDDOWNLOAD
-wget -O "2006-06/Tony Robbins: Why we do what we do (TED2006) [21m45s].mp4" http://download.ted.com/talks/TonyRobbins_2006-480p.mp4?apikey=TEDDOWNLOAD
 wget -O "2006-06/Al Gore: Averting the climate crisis (TED2006) [16m17s].mp4" http://download.ted.com/talks/AlGore_2006-480p.mp4?apikey=TEDDOWNLOAD
-wget -O "2006-06/David Pogue: Simplicity sells (TED2006) [21m26s].mp4" http://download.ted.com/talks/DavidPogue_2006-480p.mp4?apikey=TEDDOWNLOAD
 wget -O "2006-06/Majora Carter: Greening the ghetto (TED2006) [18m36s].mp4" http://download.ted.com/talks/MajoraCarter_2006-480p.mp4?apikey=TEDDOWNLOAD
 wget -O "2006-06/Ken Robinson: How schools kill creativity (TED2006) [19m24s].mp4" http://download.ted.com/talks/SirKenRobinson_2006-480p.mp4?apikey=TEDDOWNLOAD
