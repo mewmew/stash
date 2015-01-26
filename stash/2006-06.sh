9ad2395a3801f1cb182817815f86d2ee5cfef3db
mewmew <rnd0x00@gmail.com>
2015-01-26 15:20:35 +0100

    Watch "Al Gore: Averting the climate crisis".

--- stash/2006-06.sh
+++ stash/2006-06.sh
@@ -1,4 +1,3 @@
 mkdir -p "2006-06"
 wget -O "2006-06/Hans Rosling: The best stats you've ever seen (TED2006) [19m50s].mp4" http://download.ted.com/talks/HansRosling_2006-480p.mp4?apikey=TEDDOWNLOAD
-wget -O "2006-06/Al Gore: Averting the climate crisis (TED2006) [16m17s].mp4" http://download.ted.com/talks/AlGore_2006-480p.mp4?apikey=TEDDOWNLOAD
 wget -O "2006-06/Ken Robinson: How schools kill creativity (TED2006) [19m24s].mp4" http://download.ted.com/talks/SirKenRobinson_2006-480p.mp4?apikey=TEDDOWNLOAD
